672d51598be98cee59d610168b385fbbe16cf093
mallone.nogueira <mallone.nogueira@SHX0532-LINUX.shxinfor.local>
2020-02-11 16:54:59 -0300

    Updates

---
+++ precache-manifest.62e4607e5b130c892dfc9278d8fa6625.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0424da1d7b2df858a0bd",
+    "url": "/React-export-image/static/js/runtime~main.0424da1d.js"
+  },
+  {
+    "revision": "5ea3ac3194ac2b40ca19",
+    "url": "/React-export-image/static/js/main.5ea3ac31.chunk.js"
+  },
+  {
+    "revision": "681044385baf94033c2b",
+    "url": "/React-export-image/static/js/1.68104438.chunk.js"
+  },
+  {
+    "revision": "5ea3ac3194ac2b40ca19",
+    "url": "/React-export-image/static/css/main.1a45ba0e.chunk.css"
+  },
+  {
+    "revision": "b8d3b449afb8ecb401e4550b6e6a8620",
+    "url": "/React-export-image/index.html"
+  }
+];
